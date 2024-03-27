a6630950f02c94d90479a5da8d0e68e216dc237e
can <can@anyscale.com>
2024-03-27 16:30:53 +0000

    [ci] separate bucket for test data on pr and master
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/automation/weekly_green_metric.py
+++ ci/ray_ci/automation/weekly_green_metric.py
@@ -41,7 +41,7 @@ def main(production: bool, check: bool) -> None:
 
     if production:
         boto3.client("s3").put_object(
-            Bucket=get_global_config()["state_machine_aws_bucket"],
+            Bucket=get_global_config()["state_machine_master_aws_bucket"],
             Key=f"{AWS_WEEKLY_GREEN_METRIC}/blocker_{int(time.time() * 1000)}.json",
             Body=json.dumps(num_blocker_by_team),
         )
